57f261d3620c7a6596c71818028589d4250f5512
mbc <malcolmbalch@gmail.com>
2017-10-07 16:37:24 -0400

    gofmt

--- main.go
+++ main.go
@@ -1,162 +1,162 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  "encoding/json"
-  "os"
-  "time"
-  "net/http"
-  "html/template"
-  "strconv"
-  "io/ioutil"
-  _ "github.com/lib/pq"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 // Global configuration contants
 var Conf Configuration = Configuration{}
 
 type AnimalImage struct {
-  Location string
+	Location string
 }
 
 type Configuration struct {
-  Host     string
-  Port     int
-  User     string
-  Password string
-  Dbname   string
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Dbname   string
 }
 
 func main() {
-  // Get consts
-  file, _ := os.Open("conf.json")
-  decoder := json.NewDecoder(file)
-  err := decoder.Decode(&Conf)
-  if err != nil {
-    fmt.Println("error:", err)
-  }
+	// Get consts
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+	err := decoder.Decode(&Conf)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
 
-  // Training page
-  http.HandleFunc("/train/", home)
+	// Training page
+	http.HandleFunc("/train/", home)
 
-  // Next image
-  http.HandleFunc("/next_image/", nextImage)
+	// Next image
+	http.HandleFunc("/next_image/", nextImage)
 
-  // Preference event
-  http.HandleFunc("/preference_event/", preferenceEvent)
+	// Preference event
+	http.HandleFunc("/preference_event/", preferenceEvent)
 
-  // Static files
-  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// Static files
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-  // Server
-  http.ListenAndServe(":8080", nil)
+	// Server
+	http.ListenAndServe(":8080", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("home.html")
-  // TODO: execute without param
-  t.Execute(w, "https://i.imgur.com/WQBMaDU.jpg")
+	t, _ := template.ParseFiles("home.html")
+	// TODO: execute without param
+	t.Execute(w, "https://i.imgur.com/WQBMaDU.jpg")
 }
 
 // Response object for the next_image api endpoint
 type NextImageResponse struct {
-  ImageUrl string
-  ImageId string
+	ImageUrl string
+	ImageId  string
 }
 
 // Return a new image for the user
 func nextImage(w http.ResponseWriter, r *http.Request) {
 
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    Conf.Host, Conf.Port, Conf.User, Conf.Password, Conf.Dbname)
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		Conf.Host, Conf.Port, Conf.User, Conf.Password, Conf.Dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("Successfully connected to pg")
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected to pg")
 
-  // TODO: Join against preference_events to only get unseen images
-  sqlStatement := `  
+	// TODO: Join against preference_events to only get unseen images
+	sqlStatement := `  
   SELECT * FROM images LIMIT 1
   `
-  rows, err := db.Query(sqlStatement);
-  if err != nil {
-    panic(err)
-  }
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		panic(err)
+	}
 
-  var nextImage string
-  var imageIdStr string;
-  for rows.Next() {
-    var imageId int
-    var original_url string
-    var timestamp time.Time
-    err = rows.Scan(&imageId, &nextImage, &original_url, &timestamp)
-    if err != nil {
-      panic(err)
-    }
-    imageIdStr = strconv.Itoa(imageId)
-    if err != nil {
-      panic(err)
-    }
-  }
+	var nextImage string
+	var imageIdStr string
+	for rows.Next() {
+		var imageId int
+		var original_url string
+		var timestamp time.Time
+		err = rows.Scan(&imageId, &nextImage, &original_url, &timestamp)
+		if err != nil {
+			panic(err)
+		}
+		imageIdStr = strconv.Itoa(imageId)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-  resp := NextImageResponse{nextImage, imageIdStr}
-  js, err := json.Marshal(resp)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(js)
+	resp := NextImageResponse{nextImage, imageIdStr}
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 // Struct for decoding preference request parameters
 type PreferenceParams struct {
-  Liked string
-  ImageId string
+	Liked   string
+	ImageId string
 }
 
 // When a user likes or dislikes an image, we write the event to a database
 func preferenceEvent(w http.ResponseWriter, r *http.Request) {
-  body, err := ioutil.ReadAll(r.Body)
-  if err != nil {
-    panic(err)
-  }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
 
-  var p PreferenceParams
-  err = json.Unmarshal(body, &p)
-  if err != nil {
-    panic(err)
-  }
+	var p PreferenceParams
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		panic(err)
+	}
 
-  // Connect to database
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    Conf.Host, Conf.Port, Conf.User, Conf.Password, Conf.Dbname)
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
+	// Connect to database
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		Conf.Host, Conf.Port, Conf.User, Conf.Password, Conf.Dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
-  sqlStatement := `  
+	sqlStatement := `  
   INSERT INTO preference_events (user_id, image_id, liked)
   VALUES ($1, $2, $3)`
-  _, err = db.Exec(sqlStatement, 1, p.ImageId, p.Liked)
-  if err != nil {
-    panic(err)
-  }
+	_, err = db.Exec(sqlStatement, 1, p.ImageId, p.Liked)
+	if err != nil {
+		panic(err)
+	}
 }
