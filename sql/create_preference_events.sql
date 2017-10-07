118f9f50b42d79e891f409be8031940262d0978c
mbc <malcolmbalch@gmail.com>
2017-10-07 13:40:50 -0400

    remove pkey on preference_event

--- sql/create_preference_events.sql
+++ sql/create_preference_events.sql
@@ -1,5 +1,5 @@
 CREATE TABLE preference_events(
-  user_id int primary key not null, 
+  user_id int not null, 
   image_id int not null, 
   liked boolean not null, 
   timestamp timestamp default current_timestamp
